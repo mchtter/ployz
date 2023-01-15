e537cba69d77d3542b9a64e205125145af9083ae
Alperen ERYILMAZ <mchtter@gmail.com>
2023-01-16 00:26:28 +0300

    develop: homepage and home table cell created

--- ployz/Scene/Homepage/View/GamesTableViewCell.swift
+++ ployz/Scene/Homepage/View/GamesTableViewCell.swift
@@ -5,4 +5,15 @@
 //  Created by Mücahit Alperen Eryılmaz on 15.01.2023.
 //
 
-import Foundation
+import UIKit
+
+final class GamesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var gameImage: UIImageView!
+    @IBOutlet weak var gameTitle: UILabel!
+    @IBOutlet weak var gameDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- ployz/Scene/Homepage/View/HomepageViewController.swift
+++ ployz/Scene/Homepage/View/HomepageViewController.swift
@@ -7,6 +7,33 @@
 
 import UIKit
 
-class HomepageView: UIViewController {
+class HomepageViewController: UIViewController {
+    
+    @IBOutlet weak var gamesTableView: UITableView! {
+        didSet {
+            gamesTableView.delegate = self
+            gamesTableView.dataSource = self
+            gamesTableView.register(UINib(nibName: "GamesTableViewCell", bundle: nil), forCellReuseIdentifier: "gamesTableCell")
+            gamesTableView.rowHeight = 161
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension HomepageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gamesTableCell", for: indexPath)
+        
+        
+        return cell
+    }
+    
     
 }
