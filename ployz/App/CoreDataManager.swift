326277a1a9f93f8db15762160b10f3faa39324df
Alperen ERYILMAZ <mchtter@gmail.com>
2023-01-23 00:49:49 +0300

    develop: favorites screen created and handler settings is done in detail page

---
+++ ployz/App/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  ployz
+//
+//  Created by Mücahit Alperen Eryılmaz on 22.01.2023.
+//
+
+import Foundation
---
+++ ployz/App/GlobalVariables.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalVariables.swift
+//  ployz
+//
+//  Created by Mücahit Alperen Eryılmaz on 22.01.2023.
+//
+
+import Foundation
